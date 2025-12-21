3808c278974bb73bececf3077a8bd434e595c480
Cooper French <cooperfrench95@gmail.com>
2025-12-21 13:58:16 +0700

    coopyswap wip

---
+++ NOTES.md
@@ -0,0 +1,20 @@
+# Simple AMM Implementation
+
+The goal is to create an interface like Uniswap V2. 
+
+## User Stories
+
+* As a user of CoopySwap, I want to be able to create a liquidity pool between two ERC20s.
+* As a user of CoopySwap, I want to be able to create a liquidity pool between ETH and an ERC20.
+* As a user of CoopySwap, I want to be able to perform a swap between two tokens on a liquidity pool.
+* As a user of CoopySwap, I expect the current price of each token in the liquidity pool to adjust automatically based on the contents of the liquidity pool.
+* As a user of CoopySwap, I want to be able to provide liquidity to a liquidity pool.
+* As a user of CoopySwap, I should be given an NFT representing my proportion of holdings within the LP when I provide liquidity.
+* As a user of CoopySwap, I should be able to turn in the NFT representation of my LP holdings in exchange for withdrawing my liquidity from the pool
+* As a user of CoopySwap, I should receive fee benefits when I turn in my NFT in addition to my liquidity.
+
+## Technical Requirements
+
+* Contract that creates LP and handles withdrawals & deposits to that LP as well as the associated NFTs.
+* Contract representing an LP itself, manages internal LP state and fee collection
+
---
+++ src/AMM.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./LiquidityPool.sol";
+
+contract CoopySwapAMMManager {
+
+    error PairAlreadyExistsError(string message, address existingLP);
+
+    // Storage for existing pools
+    mapping(
+      address => mapping(
+        address => address
+      )
+    ) public existingPairs;
+
+    function initializeLP(
+        address token1,
+        address token2
+    ) public returns (address) {
+        // Sort the addresses in a deterministic order before hashing
+        // This is intended to prevent things like creating a USD/ETH pair when an ETH/USD pair already exists
+        (address firstToken, address secondToken) = token1 < token2 ? (token1, token2) : (token2, token1);
+        
+        // Hash the token addresses 
+        bytes memory pairBytes = abi.encodePacked(firstToken, secondToken);
+        bytes32 tokenPairHash = keccak256(pairBytes);
+
+        // Check for an existing pair
+        bool pairAlreadyExists = existingPairs[firstToken][secondToken] == address(0);
+
+        if (pairAlreadyExists) {
+          revert PairAlreadyExistsError('That liquidity pool already exists!', existingPairs[firstToken][secondToken]);
+        }
+
+        // Initialize LP and store its new address        
+        address newLP = address(new CoopySwapLiquidityPool{ salt: tokenPairHash }(firstToken, secondToken));
+
+        existingPairs[firstToken][secondToken] = newLP;
+        existingPairs[secondToken][firstToken] = newLP;
+
+        return newLP;
+    }
+}
