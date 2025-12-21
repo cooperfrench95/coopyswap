bc887cae6bbd048ef0a95c8f86009cee4fe6ed6c
Cooper French <cooperfrench95@gmail.com>
2025-12-21 14:05:20 +0700

    lint

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract CoopySwapLiquidityPool is ERC20Permit {
+contract CoopySwapLiquidityPool {
   address token1;
   address token2;
 
@@ -19,12 +18,12 @@ contract CoopySwapLiquidityPool is ERC20Permit {
 
   uint8 constant SCALE_ZEROES = 18;
   uint8 constant MAX_SLIPPAGE_BPS = 200;
-  uint8 constant SLIPPAGE_BPS_DENOMINATOR = 10_000;
+  uint16 constant SLIPPAGE_BPS_DENOMINATOR = 10_000;
 
   struct LiquidityPosition {
     uint256 token1Amount;
     uint256 token2Amount;
-    date liquidityAdded;
+    uint256 liquidityAddedAt; // unix timestamp
     address owner;
   }
 
@@ -43,7 +42,7 @@ contract CoopySwapLiquidityPool is ERC20Permit {
     token2Decimals = getDecimals(secondToken);
   }
 
-  function provideLiquidity(uint256 firstTokenAmount, uint256 secondTokenAmount) {
+  function provideLiquidity(uint256 firstTokenAmount, uint256 secondTokenAmount) public {
     // Confirm allowance granted
     uint256 firstTokenAllowance = IERC20(token1).allowance(msg.sender, address(this));
     uint256 secondTokenAllowance = IERC20(token2).allowance(msg.sender, address(this));
@@ -57,7 +56,7 @@ contract CoopySwapLiquidityPool is ERC20Permit {
       uint256 currentPrice = calcPrice(token1Liquidity, token2Liquidity);
       uint256 userAssumedPrice = calcPrice(firstTokenAmount, secondTokenAmount);
       
-      uint256 slippage = Math.mulDiv(currentPrice, 10**SCALE_ZEROES, assumedPrice);
+      uint256 slippage = calcSlippage(currentPrice, userAssumedPrice);
 
       if (slippage > MAX_SLIPPAGE_BPS) {
         revert SlippageTooHigh();
@@ -65,35 +64,35 @@ contract CoopySwapLiquidityPool is ERC20Permit {
     }
   }
 
-  function calcSlippage(uint256 currentPrice, uint256 assumedPrice) {
+  function calcSlippage(uint256 currentPrice, uint256 assumedPrice) private pure returns (uint256) {
     uint256 diff = currentPrice > assumedPrice ? currentPrice - assumedPrice : assumedPrice - currentPrice;
 
     // This does (diff * 10_000) / currentPrice
-    Math.mulDiv(diff, SLIPPAGE_BPS_DENOMINATOR, currentPrice);
+    return Math.mulDiv(diff, SLIPPAGE_BPS_DENOMINATOR, currentPrice);
   }
 
-  function getNormalizedInt(uint256 num, uint8 decimals) private returns (uint256) {
+  function getNormalizedInt(uint256 num, uint8 decimals) private pure returns (uint256) {
     if (decimals > SCALE_ZEROES) {
-      return num * 10**(SCALE_ZEROES - decimals)
+      return num * 10**(SCALE_ZEROES - decimals);
     }
     else if (decimals < SCALE_ZEROES) {
-      return num / 10**(decimals - SCALE_ZEROES)
+      return num / 10**(decimals - SCALE_ZEROES);
     }
-    return num * 10**SCALE_ZEROES
+    return num * 10**SCALE_ZEROES;
   }
 
-  function calcPrice(uint256 tokenALiquidity, uint256 tokenBLiqudity) {
-    uint256 normalisedToken1Balance = getNormalizedInt(tokenALiquidity, token1Decimals)
-    uint256 normalisedToken2Balance = getNormalizedInt(tokenBLiquidity, token2Decimals)
+  function calcPrice(uint256 tokenALiquidity, uint256 tokenBLiquidity) private view returns (uint256) {
+    uint256 normalisedToken1Balance = getNormalizedInt(tokenALiquidity, token1Decimals);
+    uint256 normalisedToken2Balance = getNormalizedInt(tokenBLiquidity, token2Decimals);
 
-    return Math.mulDiv(normalisedToken1Balance, 10**(SCALE_ZEROES), normalisedToken2Balance)
+    return Math.mulDiv(normalisedToken1Balance, 10**(SCALE_ZEROES), normalisedToken2Balance);
   }
 
-  function withdrawLiquidity() {
+  function withdrawLiquidity() public {
 
   }
 
-  function swap() {
+  function swap() public {
 
   }
 
